9bf32fab9f5d8f6709aa64c4b778b83761d4bfab
Konrad Oleksiuk <konole@gmail.com>
2016-11-04 17:52:58 +0100

    Fix tests for AuthenticationToken

--- test/models/authentication_token_test.exs
+++ test/models/authentication_token_test.exs
@@ -3,16 +3,16 @@ defmodule DevNotex.AuthenticationTokenTest do
 
   alias DevNotex.AuthenticationToken
 
-  @valid_attrs %{token: "some content"}
+  @valid_attrs %{user_id: 1}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = AuthenticationToken.changeset(%AuthenticationToken{}, @valid_attrs)
+  test "create_changeset with valid attributes" do
+    changeset = AuthenticationToken.create_changeset(%AuthenticationToken{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = AuthenticationToken.changeset(%AuthenticationToken{}, @invalid_attrs)
+  test "create_changeset with invalid attributes" do
+    changeset = AuthenticationToken.create_changeset(%AuthenticationToken{}, @invalid_attrs)
     refute changeset.valid?
   end
 end
